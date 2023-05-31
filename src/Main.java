af6d230527b7b0a4049432ad5eec624b8888e3a4
tirgani12 <ahmed.abhr6@gmail.com>
2023-05-30 21:22:22 -0400

    minor change2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        for(int i=0;i<20;i++){
+        for(int i=0;i<5;i++){
             System.out.println("hello");
             Thread.sleep(1000);
         }
